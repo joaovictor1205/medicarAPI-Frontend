aee435c3d7243f0edf429e9b563e37f8d82a9220
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-29 23:09:19 +0000

    Iniciando page/component Criar Conta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageCriarContaComponent } from './components/page/page-criar-conta/page-criar-conta.component';
+import { PageLoginComponent } from './components/page/page-login/page-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PageLoginComponent,
+  },
+  {
+    path: 'criar-conta',
+    component: PageCriarContaComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
