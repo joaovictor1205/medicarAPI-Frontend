3fea2ab5c662e3797201a63b139000939bbd099c
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-29 23:26:46 +0000

    Definindo as Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageCriarConsultaComponent } from './components/page/page-criar-consulta/page-criar-consulta.component';
 import { PageCriarContaComponent } from './components/page/page-criar-conta/page-criar-conta.component';
+import { PageHomeComponent } from './components/page/page-home/page-home.component';
 import { PageLoginComponent } from './components/page/page-login/page-login.component';
 
 const routes: Routes = [
@@ -11,7 +13,15 @@ const routes: Routes = [
   {
     path: 'criar-conta',
     component: PageCriarContaComponent,
-  }
+  },
+  {
+    path: 'home',
+    component: PageHomeComponent,
+  },
+  {
+    path: 'criar-consulta',
+    component: PageCriarConsultaComponent,
+  }, 
 ];
 
 @NgModule({
